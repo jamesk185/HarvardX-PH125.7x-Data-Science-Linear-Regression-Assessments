a29c662e65bfcf19910783ac4a3248071bd55f83
James Kowalik <jameskowalik@outlook.com>
2020-07-31 22:55:23 +0900

    1.1 finished

--- code.R
+++ code.R
@@ -1,1 +1,16 @@
 #1.1
+library(Lahman)
+library(tidyverse)
+?Teams
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(AB_per_game = AB / G, R_per_game = R / G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(win_rate = W/G, E_per_game = E/G) %>%
+  ggplot(aes(win_rate, E_per_game)) + 
+  geom_point(alpha = 0.5)
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(doubles = X2B/G, triples = X3B/G) %>%
+  ggplot(aes(doubles, triples)) + 
+  geom_point(alpha = 0.5)
